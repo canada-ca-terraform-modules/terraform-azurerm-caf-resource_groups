8ed1916c3ef757f59639a89953fedf74959d04c3
Bernard Maltais <bmaltais@gmail.com>
2020-06-28 01:44:46 +0000

    Fix misplaced -rg

--- module.tf
+++ module.tf
@@ -2,7 +2,7 @@
 resource "azurerm_resource_group" "rg" {
   for_each = var.resource_groups
 
-  name     = substr(replace("${local.env_4}-${replace(each.value.userDefinedString, "-", "_")-rg}", local.name-regex, ""), 0, 90)
+  name     = substr(replace("${local.env_4}-${replace(each.value.userDefinedString, "-", "_")}-rg", local.name-regex, ""), 0, 90)
   location = lookup(each.value, "location", null) == null ? var.location : each.value.location
   tags     = lookup(each.value, "tags", null) == null ? local.tags : merge(local.tags, each.value.tags)
 }
