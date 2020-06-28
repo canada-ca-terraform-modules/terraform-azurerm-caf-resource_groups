7a47c5f3953235f896d09df9bdd185a1d17a6945
Bernard Maltais <bmaltais@gmail.com>
2020-06-28 19:37:07 +0000

    Update

--- module.tf
+++ module.tf
@@ -2,7 +2,7 @@
 resource "azurerm_resource_group" "rg" {
   for_each = var.resource_groups
 
-  name     = local.names[each.key]
+  name     = local.names[each.value.userDefinedString]
   location = lookup(each.value, "location", null) == null ? var.location : each.value.location
   tags     = lookup(each.value, "tags", null) == null ? local.tags : merge(local.tags, each.value.tags)
 }
