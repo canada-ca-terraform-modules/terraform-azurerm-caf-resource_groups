32e0a0c3a258c5285556a2bdac4ac1056b9be459
Maxime Mahdavian <maxime.mahdavian@ssc-spc.gc.ca>
2024-08-02 13:11:53 -0400

    outputs have descriptions and ESLZ template files added

--- output.tf
+++ output.tf
@@ -1,20 +1,24 @@
-output "object" {
-  description = "Returns the resource group objects created"
+output "resource-group-object" {
+  description = "Returns the resource group object created"
   value = azurerm_resource_group.rg
 }
 
 output "name" {
+  description = "Returns the name of the resource group"
   value = azurerm_resource_group.rg.name
 }
 
 output "id" {
+  description = "Returns the id of the resource group"
   value = azurerm_resource_group.rg.id
 }
 
 output "location" {
+  description = "Returns the location of the resource group"
   value = azurerm_resource_group.rg.location
 }
 
 output "tags" {
+  description = "Returns the tags of the resource groups"
   value = azurerm_resource_group.rg.tags
 }
